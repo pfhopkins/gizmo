a359f79984befc44d14c8b1c34476d16e3195e79
Mike Grudic <mike.grudic@gmail.com>
2026-03-09 18:20:28 -0400

    error handling for test build

--- test/build_gizmo_for_test.sh
+++ test/build_gizmo_for_test.sh
@@ -1,5 +1,5 @@
 # Generic commands to build GIZMO for a given test
 
 cp test/$TEST_NAME/Config.sh . # retrieve the config file
-echo 'SYSTYPE="MacBookCellar"' > Makefile.systype # update makefile.systype
+echo 'SYSTYPE="Ubuntu"' > Makefile.systype # update makefile.systype
 make
