4595225c322749358c7b07249fef98b26728d845
Philip Hopkins <phopkins@caltech.edu>
2026-03-27 23:24:40 -0700

    testing

--- test/soundwave/test_soundwave.py
+++ test/soundwave/test_soundwave.py
@@ -5,8 +5,14 @@ from gizmo.test import build_and_run_test, assert_snapshots_are_close
 from os import path
 
 
+<<<<<<< Updated upstream
 @pytest.mark.parametrize("num_mpi_ranks", (12,))
 def test_soundwave(num_mpi_ranks):
+=======
+
+@pytest.mark.parametrize("num_mpi_ranks,num_omp_threads", [(8, 0), (1, 8), (2, 4)])
+def test_soundwave(num_mpi_ranks, num_omp_threads):
+>>>>>>> Stashed changes
     test_name = "soundwave"
     build_and_run_test(test_name, num_mpi_ranks)
     outputdir = f"test/{test_name}/output"
