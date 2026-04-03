d11c340dd0d6380d0dc55372eb57c5cb374db363
Shivan Khullar <shivankhullar@gmail.com>
2026-04-03 15:56:09 -0400

    function definition for refinement region center calculation

--- core/proto.h
+++ core/proto.h
@@ -51,6 +51,9 @@ double target_mass_renormalization_factor_for_mergesplit(int i, int split_key);
 int check_if_sufficient_mergesplit_time_has_passed(int i);
 int is_particle_a_special_zoom_target(int i);
 #endif
+#ifdef FLAG_BASED_REFINEMENT
+void calculate_refinement_region_center(void);
+#endif
 int merge_particles_ij(int i, int j);
 int split_particle_i(int i, int n_particles_split, int i_nearest);
 double gamma_eos(int i);
