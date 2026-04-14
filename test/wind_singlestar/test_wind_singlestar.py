474b7229ebe74eb0e346cf21b73e72a421c168ef
Mike Grudic <mike.grudic@gmail.com>
2026-04-14 11:20:43 -0400

    smaller test parameter suite

--- test/wind_singlestar/test_wind_singlestar.py
+++ test/wind_singlestar/test_wind_singlestar.py
@@ -208,11 +208,11 @@ def get_snapshots(test_name, extra_config_flags=()):
 @pytest.mark.parametrize("num_mpi_ranks,num_omp_threads", [(default_mpi_ranks(), default_omp_threads())])
 @pytest.mark.parametrize(
     "res",
-    [32, 64, 128],
+    [64,],
     ids=lambda x: f"N{x}",
 )
 @pytest.mark.parametrize(
-    "Mdot_vw", [(1e-6, 3000.0), (1e-5, 3000.0), (1e-4, 3000.0)], ids=lambda x: f"Mdot{x[0]:.0e}_vw{x[1]:.0f}"
+    "Mdot_vw", [(1e-5, 3000.0),], ids=lambda x: f"Mdot{x[0]:.0e}_vw{x[1]:.0f}"
 )
 @pytest.mark.parametrize("wind_mode", [None, 1, 2], ids=lambda x: f"wm{x}" if x else "wm_auto")
 @pytest.mark.parametrize(
